01ef3ecc66f978faafadc3b51ab7f52407c8e9c1
Shangmin Guo <shawnguo.cn@gmail.com>
2024-01-26 22:26:17 +0000

    refactored offline batchfactories w/o reweighting

--- batchfactory.py
+++ batchfactory.py
@@ -8,13 +8,15 @@ Each batch is a dictionary of the following format:
 
     'generations1': str, # text of the 1st generation
     'generations1_token_ids': tensor, # token ids of the 1st generation
-    'generations1_attention_mask': tensor, # attention mask of the 1st generation
+    'generations1_attention_mask': tensor,
+    # attention mask of the 1st generation
     'generation1_reward': float, # reward of the 1st generation
     'generation1_weight': float, # weight of the 1st generation
 
     'generations2': str, # text of the 2nd generation
     'generations2_token_ids': tensor, # token ids of the 2nd generation
-    'generations2_attention_mask': tensor, # attention mask of the 2nd generation
+    'generations2_attention_mask': tensor,
+    # attention mask of the 2nd generation
     'generation2_reward': float, # reward of the 2nd generation
     'generation2_weight': float, # weight of the 2nd generation
 }
@@ -25,6 +27,7 @@ from transformers import PreTrainedModel, AutoTokenizer
 from typing import Optional, Union
 
 import DataLoader
+from samplereweighter import SampleReweighter
 
 
 class BatchFactory:
@@ -32,10 +35,12 @@ class BatchFactory:
         self,
         dataset_name: str, # e..g ['hh', 'shp']
         tokenizer, # Huggingface tokenizer object
-        generator: Union[None, PreTrainedModel],
+        generator: Union[None, PreTrainedModel] = None,
         # None for offline data, otherwise a Huggingface model
-        annotator: Union[None, PreTrainedModel],
+        annotator: Union[None, PreTrainedModel] = None,
         # None for offline data, otherwise a Huggingface model
+        reweighter: Union[None, SampleReweighter] = None,
+        # None for on reweighting, otherwise a SampleReweighter object
         split: str = 'train',
         batch_size: int = 1,
         max_length: int = 512,
@@ -67,8 +72,7 @@ class BatchFactory:
 
         self.generator = generator
         self.annotator = annotator
-        self.online = self._get_online_flag()
-        self.pairwise = self._get_pariwise_flag()
+        self.reweighter = reweighter
 
         assert n_epochs is not None or n_examples is not None, \
             "Must specify either n_epochs or n_examples"
@@ -76,7 +80,14 @@ class BatchFactory:
         self.epoch_idx = 0
         self.n_examples = n_examples
 
+        self.online = self._get_online_flag()
+        self.pairwise = self._get_pariwise_flag()
+        self.reweight = self._get_reweight_flag()
+        self._check_type()
         self.data_loader = self._get_dataloader()
+
+    def _check_type(self):
+        raise NotImplementedError
 
     def __iter__(self):
         raise NotImplementedError
@@ -101,56 +112,31 @@ class BatchFactory:
         else:
             return False
 
+    def _get_reweight_flag(self):
+        if self.reweighter is not None:
+            return True
+        else:
+            return False
 
-class OfflinePairwiseBatchFactory(BatchFactory):
-    def __init__(
-        self,
-        dataset_name: str, # e..g ['hh', 'shp']
-        tokenizer, # Huggingface tokenizer object
-        generator: Union[None, PreTrainedModel],
-        # None for offline data, otherwise a Huggingface model
-        annotator: Union[None, PreTrainedModel],
-        # None for offline data, otherwise a Huggingface model
-        split: str = 'train',
-        batch_size: int = 1,
-        max_length: int = 512,
-        max_prompt_length: int = 128,
-        n_epochs: Optional[int] = None,
-        n_examples: Optional[int] = None,
-        human_prefix: str = '\n<|user|>\n', # marks start of human's turn
-        human_suffix: str = '', # marks end of human's turn
-         # marks start of assistant's turn
-        assistant_prefix: str = '\n<|assistant|>\n',
-        assistant_suffix: str = '',   # marks end of assistant's turn
-        seed:int = 0,
-        **kwargs
-    ):
-        super().__init__(
-            dataset_name,
-            tokenizer,
-            generator,
-            annotator,
-            split,
-            batch_size,
-            max_length,
-            max_prompt_length,
-            n_epochs,
-            n_examples,
-            human_prefix,
-            human_suffix,
-            assistant_prefix,
-            assistant_suffix,
-            seed,
-            **kwargs
-        )
+
+class OfflineBatchFactory(BatchFactory):
+    """Load offline data without reweighting.
+    """
+    def _check_type(self):
         assert self.online is False, \
-            'Cannot use OfflinePairwiseBatchFactory for online data'
+            'Cannot use OfflineBatchFactory for online data'
+        assert self.reweight is False, \
+            'Cannot use OfflineBatchFactory for reweighting. If you want to' + \
+                ' reweight, use OfflineRWBatchFactory instead'
 
     def __iter__(self):
         return self.data_loader.__iter__()
 
+    def _offline_check_type(self):
+        raise NotImplementedError
 
-class SFTBatchFactory(OfflinePairwiseBatchFactory):
+
+class SFTBatchFactory(OfflineBatchFactory):
     def _get_dataloader(self):
         return DataLoader.SFTDataLoader(
             self.dataset_name,
@@ -159,7 +145,7 @@ class SFTBatchFactory(OfflinePairwiseBatchFactory):
             self.batch_size,
             self.max_length,
             self.max_prompt_length,
-            1,
+            self.batch_size,
             self.n_epochs,
             self.n_examples,
             self.human_prefix,
@@ -170,32 +156,32 @@ class SFTBatchFactory(OfflinePairwiseBatchFactory):
         )
 
 
-class OfflinePointwiseBatchFactory(OfflinePairwiseBatchFactory):
+class OfflinePointwiseBatchFactory(OfflineBatchFactory):
     def _get_dataloader(self):
+        return DataLoader.PointwiseFeedbackDataLoader(
+            self.dataset_name,
+            self.tokenizer,
+            self.split,
+            self.batch_size,
+            self.max_length,
+            self.max_prompt_length,
+            self.batch_size,
+            self.n_epochs,
+            self.n_examples,
+            human_prefix=self.human_prefix,
+            human_suffix=self.human_suffix,
+            assistant_prefix=self.assistant_prefix,
+            assistant_suffix=self.assistant_suffix,
+            **self.kwargs
+        )
+
+    def _offline_check_type(self):
         assert self.pairwise is False, \
             'OfflinePointwiseBatchFactory cannot be used for pairwise feedback'
-        return DataLoader.PointwiseFeedbackDataLoader(
-            self.dataset_name,
-            self.tokenizer,
-            self.split,
-            self.batch_size,
-            self.max_length,
-            self.max_prompt_length,
-            1,
-            self.n_epochs,
-            self.n_examples,
-            human_prefix=self.human_prefix,
-            human_suffix=self.human_suffix,
-            assistant_prefix=self.assistant_prefix,
-            assistant_suffix=self.assistant_suffix,
-            **self.kwargs
-        )
 
 
-class OfflinePairwiseBatchFactory(OfflinePairwiseBatchFactory):
+class OfflinePairwiseBatchFactory(OfflineBatchFactory):
     def _get_dataloader(self):
-        assert self.pairwise is True, 'OfflinePairtwiseBatchFactory ' + \
-            'can only be used for pointwise feedback'
         return DataLoader.PointwiseFeedbackDataLoader(
             self.dataset_name,
             self.tokenizer,
@@ -203,7 +189,7 @@ class OfflinePairwiseBatchFactory(OfflinePairwiseBatchFactory):
             self.batch_size,
             self.max_length,
             self.max_prompt_length,
-            1,
+            self.batch_size,
             self.n_epochs,
             self.n_examples,
             human_prefix=self.human_prefix,
@@ -213,13 +199,114 @@ class OfflinePairwiseBatchFactory(OfflinePairwiseBatchFactory):
             **self.kwargs
         )
 
+    def _offline_check_type(self):
+        assert self.pairwise is True, \
+            'OfflinePairwiseBatchFactory must be used for pairwise feedback'
 
-if __name__ == '__main__':
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-    batch_factory = OfflinePairwiseBatchFactory(
-        ['hh'], tokenizer, None, None, 'test', n_examples=2
-    )
-    for batch in batch_factory:
-        print(batch)
-        break
+
+class OfflineRWBatchFactory(BatchFactory):
+    """Load offline data with reweighting.
+    """
+    def _check_type(self):
+        assert self.online is False, \
+            'Cannot use OfflineBatchFactory for online data'
+        assert self.reweight is True, \
+            'Cannot use OfflineRWBatchFactory without reweighting. ' + \
+                'If you don\'t want to reweight, use OfflineBatchFactory.'
+
+    def __iter__(self):
+        batch = next(self.data_loader.__iter__())
+        # TODO: the reweighter below is a placeholder. Replace with the
+        # actual instance of `SampleReweighter` in the future.
+        batch = self.reweighter(batch)
+        return batch
+
+    def _offline_check_type(self):
+        raise NotImplementedError
+
+
+class OfflinePointwiseRWBatchFactory(OfflineRWBatchFactory):
+    """Load offline pointwise data with reweighting.
+    """
+    def _get_dataloader(self):
+        return DataLoader.PointwiseFeedbackDataLoader(
+            self.dataset_name,
+            self.tokenizer,
+            self.split,
+            self.batch_size,
+            self.max_length,
+            self.max_prompt_length,
+            self.batch_size,
+            self.n_epochs,
+            self.n_examples,
+            human_prefix=self.human_prefix,
+            human_suffix=self.human_suffix,
+            assistant_prefix=self.assistant_prefix,
+            assistant_suffix=self.assistant_suffix,
+            **self.kwargs
+        )
+
+    def _offline_check_type(self):
+        assert self.pairwise is False, 'OfflinePointwiseRWBatchFactory' + \
+            'cannot be used for pairwise feedback'
+
+
+class OfflinePairwiseRWBatchFactory(OfflineRWBatchFactory):
+    """Load offline pairwise data with reweighting.
+    """
+    def _get_dataloader(self):
+        return DataLoader.PointwiseFeedbackDataLoader(
+            self.dataset_name,
+            self.tokenizer,
+            self.split,
+            self.batch_size,
+            self.max_length,
+            self.max_prompt_length,
+            self.batch_size,
+            self.n_epochs,
+            self.n_examples,
+            human_prefix=self.human_prefix,
+            human_suffix=self.human_suffix,
+            assistant_prefix=self.assistant_prefix,
+            assistant_suffix=self.assistant_suffix,
+            **self.kwargs
+        )
+
+    def _offline_check_type(self):
+        assert self.pairwise is True, \
+            'OfflinePairwiseBatchFactory must be used for pairwise feedback'
+
+
+class OnlineBatchFactory(BatchFactory):
+    def _check_type(self):
+       assert self.online is True, \
+            'Cannot use OfflinePairwiseBatchFactory for online data'
+
+    def _get_dataloader(self):
+        return DataLoader.PromptDataLoader(
+            self.dataset_name,
+            self.tokenizer,
+            self.split,
+            self.batch_size,
+            self.max_length,
+            self.max_prompt_length,
+            self.batch_size,
+            self.n_epochs,
+            self.n_examples,
+            self.human_prefix,
+            self.human_suffix,
+            self.assistant_prefix,
+            self.assistant_suffix,
+            **self.kwargs
+        )
+
+
+class OnlinePointwiseBatchFactory(OnlineBatchFactory):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class OnlinePairwiseBatchFactory(OnlineBatchFactory):
+    def __init__(self):
+        raise NotImplementedError
+
